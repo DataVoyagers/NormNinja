395ca08e98112aac47705a796ec6209a92209fb2
Angela Lee Su Ing <152437983+angelaleesuing@users.noreply.github.com>
2025-12-19 17:07:17 +0800

    Update ForumPost.php

--- app/Models/ForumPost.php
+++ app/Models/ForumPost.php
@@ -8,30 +8,46 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ForumPost extends Model
 {
+    // Enable factory support and soft delete functionality
     use HasFactory, SoftDeletes;
 
+    /**
+     * Fields that are allowed for mass assignment
+     */
     protected $fillable = [
-        'forum_id',
-        'user_id',
-        'parent_id',
-        'content',
+        'forum_id',   // ID of the forum this post belongs to
+        'user_id',    // ID of the user who created the post
+        'parent_id',  // ID of parent post (null if top-level post)
+        'content',    // Post content/text
     ];
 
+    /**
+     * Relationship: Post belongs to a forum
+     */
     public function forum()
     {
         return $this->belongsTo(Forum::class);
     }
 
+    /**
+     * Relationship: Post belongs to a user (author)
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Relationship: Get the parent post (for replies)
+     */
     public function parent()
     {
         return $this->belongsTo(ForumPost::class, 'parent_id');
     }
 
+    /**
+     * Relationship: Get all replies to this post
+     */
     public function replies()
     {
         return $this->hasMany(ForumPost::class, 'parent_id');
